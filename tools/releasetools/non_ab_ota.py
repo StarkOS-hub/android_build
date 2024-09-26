98a42409f3ee7c5c3df355974afc74274bc274ac
Abhishek <153516999+JigenxOhtsusuki@users.noreply.github.com>
2024-09-26 19:23:23 +0530

    build: crdroid -> StarkOS

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -215,19 +215,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   # Build info
-  script.Print("                                         ");
-  script.Print("            _____            _     _     ");
-  script.Print("           |  __ \          (_)   | |    ");
-  script.Print("   ___ _ __| |  | |_ __ ___  _  __| |    ");
-  script.Print("  / __| '__| |  | | '__/ _ \| |/ _\`|    ");
-  script.Print(" | (__| |  | |__| | | | (_) | | (_| |    ");
-  script.Print("  \___|_|  |_____/|_|_ \___/|_|\__,_|  _ ");
-  script.Print("     /\             | |         (_)   | |");
-  script.Print("    /  \   _ __   __| |_ __ ___  _  __| |");
-  script.Print("   / /\ \ | '_ \ / _\`| '__/ _ \| |/ _\`|");
-  script.Print("  / ____ \| | | | (_| | | | (_) | | (_| |");
-  script.Print(" /_/    \_\_| |_|\__,_|_|  \___/|_|\__,_|");
-  script.Print("                                         ");
+  script.Print("==========================================");
+  script.Print("   _____ _             _     ____   _____ ");
+  script.Print("  / ____| |           | |   / __ \ / ____|");
+  script.Print(" | (___ | |_ __ _ _ __| | _| |  | | (___  ");
+  script.Print("  \___ \| __/ _` | '__| |/ / |  | |\___ \ ");
+  script.Print("  ____) | || (_| | |  |   <| |__| |____) |");
+  script.Print(" |_____/ \__\__,_|_|  |_|\_\\____/|_____/ ");
+  script.Print("==========================================");
+  script.Print("       with love by JigenxOhtsusuki       ");
+  script.Print("==========================================");
 
   buildid = target_info.GetBuildProp("ro.modversion")
   androidver = target_info.GetBuildProp("ro.build.version.release")
